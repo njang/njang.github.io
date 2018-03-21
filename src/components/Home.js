d3e348de1b5ad239f157bade9572e6b51f1a6a81
njang <neo.w.jang@gmail.com>
2018-03-20 23:05:50 -0500

    Revise Services section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/fontawesome-free-solid'
+import { faJsSquare, faPython, faHtml5 } from '@fortawesome/fontawesome-free-brands'
 
 class Home extends Component {
     render() {
@@ -10,12 +10,40 @@ class Home extends Component {
                     <div className='col-8 col-sm-6 col-md-4 col-lg-3'>
                         <h1 className='text-center text-uppercase font-weight-bold'>Neo Jang</h1>
                         <h4 className='text-center font-weight-bold'>Full Stack Web Developer</h4>
-                        {/*} A results-driven and detail-oriented engineer with strong analytical & critical problem-solving skills. Experienced first-hand in application development, deployment & documentation, including requirements documentation, feedback cycles with clients and education/training of end users. Portfolio of achievements in diverse engineering fields.*/}
+                        {/* <h4 className='text-center font-weight-bold'>Both Stack Engineer</h4>*/}
+                        {/* A results-driven and detail-oriented engineer with strong analytical & critical problem-solving skills. Experienced first-hand in application development, deployment & documentation, including requirements documentation, feedback cycles with clients and education/training of end users. Portfolio of achievements in diverse engineering fields.*/}
                     </div>
                 </div>
-                <div className='services justify-content-center'>
-                    <h2>Services</h2>
-                    <FontAwesomeIcon icon="code"/>
+                <div className='services'>
+                    <h2 className='my-2'>Services</h2>
+                    <div className='row justify-content-center'>
+                        <div className='col-md-3 col-sm-6'>
+                            <span className="fa-stack fa-3x">
+                                <i className="fa fa-circle fa-stack-2x"></i>
+                                <FontAwesomeIcon icon={faHtml5} className='fa-stack-1x' />
+                            </span>
+                            <h4 className='my-2'>
+                                <strong>Custom Web Development</strong>
+                            </h4>
+                            <p>My expertise is in building entire website from the ground up, covering both front-end and back-end. All we need is your idea.</p>
+                        </div>
+                        <div className='col-md-3 col-sm-6'>
+                            <span className="fa-stack fa-3x">
+                                <i className="fa fa-circle fa-stack-2x"></i>
+                                <FontAwesomeIcon icon={faJsSquare} className='fa-stack-1x' />
+                            </span>
+                            <h4 className='my-2'>
+                                <strong>JavaScript Consulting</strong>
+                            </h4>
+                            <p>I thrive in solving JavaScript problems and can help you with your current React, Node, jQuery or vanilla JavaScript problems.</p>
+                        </div>
+                        <div className='col-md-3 col-sm-6'>
+                            <span className="fa-stack fa-3x">
+                                <i className="fa fa-circle fa-stack-2x"></i>
+                                <FontAwesomeIcon icon={faPython} className='fa-stack-1x' />
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
