1fe5f599756a38ed91ad85a8955634535a33d6bc
njang <neo.w.jang@gmail.com>
2018-03-21 13:39:24 -0500

    Fix styling of Contacts page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,19 +3,23 @@ import React, { Component } from 'react';
 class Contact extends Component {
     render() {
         return (
-            <div>
-                <div className='row justify-content-center'>
-                    <a href='https://www.github.com/njang' target='_blank' rel='noopener noreferrer'>
-                        {/*<img className='col' src='images/icons/github.svg' height='48px' alt='Github' />*/}
-                        <i className='col fab fa-github h1'></i>
-                    </a>
+            <div className='content contacts'>
+                <div className='row align-items-center justify-content-center'>
                     <a href='https://www.linkedin.com/in/neojang' target='_blank' rel='noopener noreferrer'>
                         {/*<img className='col' src='images/icons/linkedin.svg' height='48px' alt='LinkedIn' />*/}
-                        <i className='col fab fa-linkedin h1'></i>
+                        <i className='col fab fa-linkedin text-dark'></i>
+                    </a>
+                    <a href='https://www.github.com/njang' target='_blank' rel='noopener noreferrer'>
+                        {/*<img className='col' src='images/icons/github.svg' height='48px' alt='Github' />*/}
+                        <i className='col fab fa-github text-dark'></i>
+                    </a>
+                    <a href='https://medium.com/@neo.w.jang' target='_blank' rel='noopener noreferrer'>
+                        {/*<img className='col' src='images/icons/gmail.svg' height='48px' alt='Gmail' />*/}
+                        <i className='col fab fa-medium text-dark'></i>
                     </a>
                     <a href='mailto:neo.w.jang@gmail.com'>
                         {/*<img className='col' src='images/icons/gmail.svg' height='48px' alt='Gmail' />*/}
-                        <i className='col fas fa-envelope h1'></i>
+                        <i className='col fas fa-envelope text-dark'></i>
                     </a>
                 </div>
             </div>
