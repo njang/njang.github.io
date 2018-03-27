719da9387209cdf73bd784188ae2ab08a1cb7bae
njang <neo.w.jang@gmail.com>
2018-03-27 15:40:26 -0500

    Optimize performance

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
 	render() {
 		return (
 			<footer className='py-3 bg-dark'>
-				<p className='m-0 text-center text-white'>Handcrafted with <span className='text-danger'>&#10084;</span></p>
+				<p className='m-0 text-center text-white'>Handcrafted with <span className='text-white'>&#10084;</span></p>
 				<p className='m-0 text-center text-white'>&copy; Neo W. Jang 2017-2018</p>
 			</footer>
 		);
